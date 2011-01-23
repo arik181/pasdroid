8ec430e540e8c7b3ed6568a89676a89f9f54a18b
unknown <arik181@.(none)>
2011-01-22 17:59:52 -0800

    Modified the layout of the main activity

--- src/net/arik181/pasdroid/Interface.java
+++ src/net/arik181/pasdroid/Interface.java
@@ -31,39 +31,108 @@ package net.arik181.pasdroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.ClipboardManager;
+import android.view.MenuInflater;
 import android.view.View;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import net.arik181.pasdroid.PasswordMaker;
 
 
-public class Interface extends Activity {
-    /** Called when the activity is first created. */
+public class Interface extends Activity 
+{
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        setGenerateButton();
+        
+        Spinner save_password_spinner = (Spinner)findViewById(R.id.save_password);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.save_password_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        save_password_spinner.setAdapter(adapter);
+        setShowButton();
+        setHideButton();
     }
 
-    public void setGenerateButton()
+    public void setShowButton()
     {
-    	Button generateButton = (Button)findViewById(R.id.generate);
+    	Button showButton = (Button)findViewById(R.id.generate);
 
-    	generateButton.setOnClickListener(new View.OnClickListener() {
+    	showButton.setOnClickListener(new View.OnClickListener() 
+    	{
     		@Override
-    		public void onClick(View v) {
+    		public void onClick(View v) 
+    		{
     			TextView generatedPasswordView = (TextView)findViewById(R.id.generated_password);
     			EditText masterPasswordView = (EditText)findViewById(R.id.password);
     			EditText usedTextView = (EditText)findViewById(R.id.used_text);
-    			
     			String masterPassword = masterPasswordView.getText().toString();
-    	    	String usedText = usedTextView.getText().toString();
+    			String usedText = usedTextView.getText().toString();
+    			
     	    	PasswordMaker pm = new PasswordMaker(masterPassword, usedText);
     	    	String password = pm.generatePassword();
-    			generatedPasswordView.setText(password);
+
+    	    	generatedPasswordView.setTextSize(24);
+    	    	generatedPasswordView.setText(password);
     		}
     	});
     }
+    
+    public void setCopyButton()
+    {
+    	Button copyButton = (Button)findViewById(R.id.copy);
+
+    	copyButton.setOnClickListener(new View.OnClickListener() 
+    	{
+    		@Override
+    		public void onClick(View v) 
+    		{
+    			EditText masterPasswordView = (EditText)findViewById(R.id.password);
+    			EditText usedTextView = (EditText)findViewById(R.id.used_text);
+    			String masterPassword = masterPasswordView.getText().toString();
+    			String usedText = usedTextView.getText().toString();
+    			
+    			ClipboardManager cm = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+    	    	
+    	    	PasswordMaker pm = new PasswordMaker(masterPassword, usedText);
+    	    	String password = pm.generatePassword();
+    	    	
+    	    	cm.setText(password);
+    		}
+    	});
+    }
+    
+    public void setHideButton()
+    {
+    	Button hideButton = (Button)findViewById(R.id.hide);
+    	hideButton.setOnClickListener(new View.OnClickListener() 
+    	{
+    		@Override
+    		public void onClick(View v) 
+    		{
+    			TextView generatedPasswordView = (TextView)findViewById(R.id.generated_password);
+    			generatedPasswordView.setText("");
+    		}
+    	});
+    }
+    
+    // Menu creation
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+    	MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options, menu);
+    	return true;
+    }
+    
+    // Menu selection
+    //public boolean onOptionsItemSelected(MenuItem item)
+    //{
+    	//return true;
+    //}
 }
